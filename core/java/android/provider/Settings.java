2a5931c63e400cf3f4670c60dd9ae9280d4042fd
t-rex600 <fisch890@gmail.com>
2016-12-18 14:48:02 +0100

    Revert "MediaScanner behavior on boot (1/3)"
    This reverts commit 4fc8c946b30aafca7b9f2238e46499db12adb117.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3738,7 +3738,6 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
-<<<<<<< HEAD
          * Show or hide clock
          * 0 - hide
          * 1 - show (default)
@@ -3802,20 +3801,7 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
-        
-        /**
-         * MediaScanner behavior on boot.
-         * 0 = enabled
-         * 1 = ask (notification)
-         * 2 = disabled
-         * @hide
-         */
-        public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
-        /**
-         * Whether the phone ringtone should be played in an increasing manner
-         * @hide
-         */        
 
         /**
          * Whether volume button press shuld be treated as wake key
@@ -6606,6 +6592,7 @@ public final class Settings {
          */
         public static final String AUTOMATIC_STORAGE_MANAGER_BYTES_CLEARED =
                 "automatic_storage_manager_bytes_cleared";
+
 
         /**
          * Last run time for the automatic storage manager.
